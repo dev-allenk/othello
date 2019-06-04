6ea31f6f322f12faa48e2356294ea207f744432c
allen <revenoir7@gmail.com>
2019-06-04 16:02:11 +0900

    feat: chageTurn메소드 및 updateState메소드 작성

--- model.js
+++ model.js
@@ -2,13 +2,31 @@ class Model {
   constructor({ validator }) {
     this.state = Array(8).fill(null).map(() => Array(8).fill(0));
     this.validator = validator;
+    this.turn = 'black';
   }
   init() {
-    model.state[3][4] = 1;
-    model.state[4][3] = 1;
-    model.state[3][3] = 2;
-    model.state[4][4] = 2;
+    this.state[3][4] = 1;
+    this.state[4][3] = 1;
+    this.state[3][3] = 2;
+    this.state[4][4] = 2;
   }
+
+  changeTurn() {
+    if(this.turn === 'black') this.turn = 'white';
+    else this.turn = 'black';
+  }
+
+  updateState(input) {
+    input = input.split(' ').map(el => Number(el));
+    const row = input[0];
+    const column = input[1];
+    
+    if(this.turn === 'black') this.state[row][column] = 1;
+    else this.state[row][column] = 2;
+    
+    this.changeTurn();
+  }
+
 }
 
 module.exports = Model;
